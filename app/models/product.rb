dcfd4a1684416bfec35543e18febeb2b40e52f0c
dongyeoplee <dongyeop.lee@headflow.net>
2013-06-14 22:46:46 +0900

    line item

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,18 @@ class Product < ActiveRecord::Base
   validates :title, uniqueness: true
   validates_length_of :title, minimum: 10
   validates :image_url, allow_blank: true, format: {with: %r{\.(gif|jpg|png)$}i, message: 'must be a URL for GIF, JPG or PNG iamge.'}
+
+  has_many :line_items
+  before_destroy :line_item_empty
+  
+  private
+  
+  def line_item_empty
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
 end
