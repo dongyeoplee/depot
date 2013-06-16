83ecc0a404d8b7dd17b825ff5ad8d25a40526121
dongyeoplee <dongyeop.lee@headflow.net>
2013-06-16 19:48:38 +0900

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'active_record/fixtures'
 
+# Fixtures.create_fixtures("#{Rails.root}/test/fixtures", "products")
+ActiveRecord::Fixtures.create_fixtures("#{Rails.root}/test/fixtures", "products")
 Product.create(title: 'Programming Ruby 1.9', description: %{<p>Ruby is the fastest growing and most exciting dynamic language out there. If you need to get working programs delivered fast, you should add Ruby to your toolbox.</p>}, image_url: 'ruby.jpg', price: 49.95)
